2ee1307f2ae7a16c65c0b942f99e05e082ca669e
msalazars831 <mss831@alumnos.unican.es>
2026-03-30 00:51:53 +0200

    Update regression_kriging_model.py with fit variogram function, return attribute functions

--- src/models/regression_kriging_model.py
+++ src/models/regression_kriging_model.py
@@ -3,8 +3,9 @@ import pandas as pd
 
 from models.base_spatial_model import BaseSpatialModel
 from sklearn.linear_model import LinearRegression
-
 from pykrige.ok import OrdinaryKriging
+
+from models.variogram_analysis import VariogramAnalysis
 
 
 class RegressionKrigingModel(BaseSpatialModel):
@@ -12,62 +13,123 @@ class RegressionKrigingModel(BaseSpatialModel):
     def __init__(
         self,
         regression_model=None,
-        variogram_model="spherical"
+        variogram_model=None,
+        variogram_params=None
     ):
         
         if regression_model is None:
             regression_model = LinearRegression()
 
         self.regression_model = regression_model
+
+        # variograma fijo (para CV)
         self.variogram_model = variogram_model
-        
+        self.variogram_params = variogram_params
+
         self.kriging_model = None
 
+    def variogram_model(self):
+        return self.variogram_model
+    
+    def variogram_params(self):
+        return self.variogram_params
 
-    def fit(self, X, y, coords):
-        """
-        X: variables explicativas
-        y: variable objetivo
-        coords: array Nx2 [lon, lat]
-        """
+    def fit_variogram(self, X, y, coords):
 
-        # 1. entrenar regresión
+        # regresión global
+        # np.isnan(X).any()
+        # np.isnan(X).sum()
+        # np.argwhere(np.isnan(X))
         self.regression_model.fit(X, y)
-
-        # 2. predicción regresión
         y_pred = self.regression_model.predict(X)
 
-        # 3. residuos
         residuals = y - y_pred
 
         lon = coords[:, 0]
         lat = coords[:, 1]
 
-        # 4. kriging sobre residuos
+        df_vario = pd.DataFrame({
+            "lon": lon,
+            "lat": lat,
+            "residuals": residuals
+        })
+
+        variogram_analysis = VariogramAnalysis(
+            df_vario,
+            x_col="lon",
+            y_col="lat",
+            value_col="residuals"
+        )
+
+        variogram_analysis.compute_experimental_variogram()
+        results, best_variogram = variogram_analysis.compare_models()
+
+        # best_variogram = results.iloc[0]
+        self.variogram_model = best_variogram["model"]
+        self.variogram_params = {
+            "sill": best_variogram["sill"],
+            "range": best_variogram["range"],
+            "nugget": best_variogram["nugget"]
+        }
+
+        print("Selected variogram model:", self.variogram_model)
+        print(results)
+
+
+    def fit(self, X, y, coords):
+
+        if self.variogram_model is None or self.variogram_params is None:
+            raise ValueError("Primero debes ejecutar fit_variogram()")
+
+        # regresión
+        self.regression_model.fit(X, y)
+        y_pred = self.regression_model.predict(X)
+
+        residuals = y - y_pred
+
+        lon = coords[:, 0]
+        lat = coords[:, 1]
+
+        # kriging con variograma FIJO
         self.kriging_model = OrdinaryKriging(
             lon,
             lat,
             residuals,
             variogram_model=self.variogram_model,
+            variogram_parameters=self.variogram_params,
             verbose=False,
             enable_plotting=False
         )
 
 
-    def predict(self, X_new, coords_new):
+    def predict(self, X_new, coords_new, type="points"):
 
-        # 1. predicción regresión
+        if self.kriging_model is None:
+            raise ValueError("El modelo no está entrenado")
+
+        # -----------------------------
+        # 1. Predicción regresión
+        # -----------------------------
         reg_pred = self.regression_model.predict(X_new)
 
+        # -----------------------------
+        # 2. Kriging residuos
+        # -----------------------------
         lon = coords_new[:, 0]
         lat = coords_new[:, 1]
 
-        # 2. kriging residuos
         krig_res, _ = self.kriging_model.execute(
-            "points",
+            type,
             lon,
             lat
         )
 
-        # 3. suma
+        # asegurar formato correcto
+        krig_res = np.array(krig_res).ravel()
+
+        # -----------------------------
+        # 3. Suma final
+        # -----------------------------
+        assert reg_pred.shape == krig_res.shape
+
         return reg_pred + krig_res
